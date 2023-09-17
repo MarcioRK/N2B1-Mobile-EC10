6d30696bcaccbbaaa069394fa5e829aa32d15ebb
MarcioRK <marcio_rios_kunimura@hotmail.com>
2023-09-17 17:11:26 -0300

    add sqLite

--- screens/ListPizzas/ListPizzas.js
+++ screens/ListPizzas/ListPizzas.js
@@ -1,10 +1,72 @@
-import { Text, View } from 'react-native';
+import { Text, View, Button, TouchableOpacity } from 'react-native';
 import styles from './styles';
+import { useFocusEffect } from '@react-navigation/native';
 
-export default function ListPizzas() {
+import {
+  createTable,
+  createRecord,
+  getAllPizzas,
+  addContact,
+  updateContact,
+  deleteContact,
+  deleteAllContacts,
+} from '../../services/dbservices';
+import { useState, useEffect, Alert, useCallback } from 'react';
+
+
+export default function ListPizzas({addPizza}) {
+
+  const [pizzas, setPizzas] = useState([]);
+
+  async function loadPizzas() {
+    try {
+      const pizzasList = await getAllPizzas();
+      setPizzas(pizzasList);
+    } catch (e) {
+      Alert.alert(e.toString());
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      loadPizzas();
+      return () => {}; // função de retorno opcional para limpar efeitos colaterais.
+    }, [])
+  );
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>ListPizzas!</Text>
+      
+      {pizzas && pizzas.length > 0 ? (
+        <View>
+          {pizzas.map((pizza, index) => (
+            <View key={index}>
+              <Text>ID: {pizza.id}</Text>
+              <Text>Name: {pizza.name}</Text>
+              <Text>Description: {pizza.description}</Text>
+            </View>
+          ))}
+        </View>
+      ) : (
+        <Text>No pizzas available.</Text>
+      )}
+
+
+      <TouchableOpacity style={styles.botao} onPress={() => loadPizzas()}>
+        <Text style={styles.textoBotao}>Recarregar Teste</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.botao} onPress={() => createTable()}>
+        <Text style={styles.textoBotao}>createTable Teste</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.botao} onPress={() => deleteAllContacts()}>
+        <Text style={styles.textoBotao}>deleteAllContacts Teste</Text>
+      </TouchableOpacity>
+
+
+      
     </View>
   );
 }
--- screens/RegisterPizza/RegisterPizza.js
+++ screens/RegisterPizza/RegisterPizza.js
@@ -1,10 +1,58 @@
-import { Text, View } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity } from 'react-native';
 import styles from './styles';
+import { useState, useEffect, Alert } from 'react';
+import { addContact } from '../../services/dbservices';
 
 export default function RegisterPizza() {
+  const [id, setId] = useState(null);
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+
+  function createUniqueId() {
+    return Date.now();
+  }
+
+
+
+  async function salvaDados() {
+    let novoRegistro = id == undefined;
+
+    let obj = {
+      id: createUniqueId(),
+      name: name,
+      description: description,
+    };
+
+    try {
+      if (novoRegistro) {
+        let resposta = await addContact(obj);
+        if (resposta)
+          Alert.alert('adicionado com sucesso!');
+        else
+          Alert.alert('Falhou miseravelmente!');
+      }
+    } catch (e) {
+      Alert.alert(e);
+    }
+  }
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>RegisterPizza!</Text>
+
+      <TextInput
+        onChangeText={(texto) => setName(texto)}
+        value={name}
+      />
+
+      <TextInput
+        onChangeText={(texto) => setDescription(texto)}
+        value={description}
+      />
+
+      <TouchableOpacity style={styles.botao} onPress={() => salvaDados()}>
+        <Text style={styles.textoBotao}>Salvar Pizza</Text>
+      </TouchableOpacity>
     </View>
   );
 }
