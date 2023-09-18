ce2d85020f43fd9863a48997ef4aa9b66aa69219
William Massao <william.massao.marubayashi@gmail.com>
2023-09-17 22:56:41 -0300

    Implementacao da RegisterPizza

--- screens/RegisterPizza/RegisterPizza.js
+++ screens/RegisterPizza/RegisterPizza.js
@@ -1,55 +1,72 @@
-<<<<<<< HEAD
-// RegisterPizza.js
-
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, Alert} from 'react-native';
+import { Text, View, TextInput, TouchableOpacity, Alert } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
-import styles from './styles'; // Importe o arquivo de estilos diretamente
+import styles from './styles';
+import { createPizzasTable, addPizza } from '../../services/dbservices';
 
 export default function RegisterPizza() {
-  const [codigo, setCodigo] = useState('');
-  const [descricao, setDescricao] = useState('');
-  const [preco, setPreco] = useState('');
+  const [id, setId] = useState(null);
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
   const [categoriaSelecionada, setCategoriaSelecionada] = useState('');
   const categorias = ['Categoria 1', 'Categoria 2', 'Categoria 3']; // Substitua com as categorias reais da pizza que você deseja consumir
 
-  const handleCadastro = () => {
-    if (!codigo || !descricao || !preco || !categoriaSelecionada) {
-      Alert.alert('Erro', 'Por favor, preencha todos os campos.');
-    } else {
-      // Aqui você pode enviar os dados para o backend ou realizar outra ação desejada
-      Alert.alert('Sucesso', 'Pizza cadastrada com sucesso!');
-      // Limpar os campos após o cadastro
-      setCodigo('');
-      setDescricao('');
-      setPreco('');
-      setCategoriaSelecionada('');
+  function createUniqueId() {
+    return Date.now();
+  }
+
+  async function salvaDados() {
+    if (!name || !description || !categoriaSelecionada) {
+      Alert.alert('Erro', 'Por favor, preencha todos os campos, incluindo a categoria.');
+      return;
     }
-  };
-  
+
+    let novoRegistro = id === null;
+
+    let obj = {
+      id: novoRegistro ? createUniqueId() : id,
+      name: name,
+      description: description,
+      categoria: categoriaSelecionada,
+    };
+    
+    try {
+      createPizzasTable()
+      if (novoRegistro) {
+        let resposta = await addPizza(obj);
+        if (resposta) {
+          Alert.alert('Sucesso', 'Pizza cadastrada com sucesso!');
+          console.log("cadastrou a pizza")
+          console.log(obj)
+          setName('');
+          setDescription('');
+          setCategoriaSelecionada('');
+          setId(null); // Limpar o campo "Id" após o cadastro
+        } else {
+          Alert.alert('Erro', 'Falha ao cadastrar a pizza.');
+        }
+      }
+    } catch (e) {
+      Alert.alert('Erro', e.toString());
+    }
+  }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.label}>Código:</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={styles.title}>Registre sua Pizza!</Text>
+
       <TextInput
+        placeholder="Nome"
+        onChangeText={(texto) => setName(texto)}
+        value={name}
         style={styles.input}
-        value={codigo}
-        onChangeText={(text) => setCodigo(text)}
       />
 
-      <Text style={styles.label}>Descrição:</Text>
       <TextInput
+        placeholder="Descrição"
+        onChangeText={(texto) => setDescription(texto)}
+        value={description}
         style={styles.input}
-        value={descricao}
-        onChangeText={(text) => setDescricao(text)}
-      />
-
-      <Text style={styles.label}>Preço Unitário:</Text>
-      <TextInput
-        style={styles.input}
-        value={preco}
-        onChangeText={(text) => setPreco(text)}
-        keyboardType="numeric"
       />
 
       <Text style={styles.label}>Categoria:</Text>
@@ -64,64 +81,8 @@ export default function RegisterPizza() {
         ))}
       </Picker>
 
-      <TouchableOpacity style={styles.button} onPress={handleCadastro}>
-        <Text style={styles.buttonText}>Cadastrar Pizza</Text>
-=======
-import { Text, View, TextInput, TouchableOpacity } from 'react-native';
-import styles from './styles';
-import { useState, useEffect, Alert } from 'react';
-import { addContact } from '../../services/dbservices';
-
-export default function RegisterPizza() {
-  const [id, setId] = useState(null);
-  const [name, setName] = useState('');
-  const [description, setDescription] = useState('');
-
-  function createUniqueId() {
-    return Date.now();
-  }
-
-
-
-  async function salvaDados() {
-    let novoRegistro = id == undefined;
-
-    let obj = {
-      id: createUniqueId(),
-      name: name,
-      description: description,
-    };
-
-    try {
-      if (novoRegistro) {
-        let resposta = await addContact(obj);
-        if (resposta)
-          Alert.alert('adicionado com sucesso!');
-        else
-          Alert.alert('Falhou miseravelmente!');
-      }
-    } catch (e) {
-      Alert.alert(e);
-    }
-  }
-
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>RegisterPizza!</Text>
-
-      <TextInput
-        onChangeText={(texto) => setName(texto)}
-        value={name}
-      />
-
-      <TextInput
-        onChangeText={(texto) => setDescription(texto)}
-        value={description}
-      />
-
-      <TouchableOpacity style={styles.botao} onPress={() => salvaDados()}>
-        <Text style={styles.textoBotao}>Salvar Pizza</Text>
->>>>>>> 1768384f59edb3914ecce1592c0548d6dc2feb9b
+      <TouchableOpacity style={styles.button} onPress={() => salvaDados()}>
+        <Text style={styles.buttonText}>Salvar Pizza</Text>
       </TouchableOpacity>
     </View>
   );
--- screens/RegisterPizza/styles.js
+++ screens/RegisterPizza/styles.js
@@ -1,35 +1,52 @@
-import {StyleSheet, StatusBar} from 'react-native';
+import { StyleSheet } from 'react-native';
 
-export const styles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16,
-    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff', // Cor de fundo branca
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    borderColor: '#ccc', // Cor da borda cinza
+    borderWidth: 1,
+    marginBottom: 20,
+    padding: 10,
+    borderRadius: 8, // Borda arredondada
   },
   label: {
     fontSize: 18,
-    fontWeight: 'bold',
     marginBottom: 8,
+    fontWeight: 'bold',
   },
-  input: {
-    fontSize: 16,
+  picker: {
+    width: '80%',
+    height: 40,
+    marginBottom: 20,
+    borderColor: '#ccc', // Cor da borda cinza
     borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 4,
-    padding: 8,
-    marginBottom: 16,
+    borderRadius: 8, // Borda arredondada
   },
   button: {
-    backgroundColor: 'blue',
-    borderRadius: 4,
-    padding: 12,
-    alignItems: 'center',
+    backgroundColor: '#FF6347', // Cor laranja (ou outra de sua escolha)
+    width: '80%', // Largura definida
+    height: 50, // Altura definida
+    justifyContent: 'center', // Centralizar conteúdo verticalmente
+    alignItems: 'center', // Centralizar conteúdo horizontalmente
+    borderRadius: 8,
   },
   buttonText: {
-    color: 'white',
-    fontSize: 18,
+    color: '#fff', // Cor do texto branco
+    fontSize: 16,
     fontWeight: 'bold',
   },
 });
-  
-  export default styles;
+
+export default styles;
