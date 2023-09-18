2ecb37860a8c125bb9c8d9021c76be4cda4d363d
MarcioRK <marcio_rios_kunimura@hotmail.com>
2023-09-18 01:06:37 -0300

    Add create order

--- screens/ListSells/ListSells.js
+++ screens/ListSells/ListSells.js
@@ -1,10 +1,129 @@
-import { Text, View } from 'react-native';
+import React, { useState, useEffect, useCallback } from 'react';
+import { Text, View, FlatList, Button, Alert, TouchableOpacity } from 'react-native';
 import styles from './styles';
+import { getOrders, deleteOrder, saveOrder, deleteAllOrders, getOrderById } from '../../services/dbservices';
+import { useFocusEffect } from '@react-navigation/native';
 
-export default function ListSells() {
+
+
+export default function ListSells({ navigation }) {
+  const [orders, setOrders] = useState([]);
+
+  async function loadOrders() {
+    try {
+      const fetchedOrders = await getOrders();
+      console.log("[loadOrders] fetchedOrders ", fetchedOrders);
+      setOrders(fetchedOrders);
+    } catch (error) {
+      Alert.alert("Erro ao recuperar pedidos", error.toString());
+    }
+  }
+
+  async function handleDeleteOrder(orderId) {
+    console.log("[handleDeleteOrder] orderId ", orderId);
+    try {
+      const success = await deleteOrder(orderId);
+      if (success) {
+        Alert.alert("Pedido deletado com sucesso!");
+        loadOrders(); // Recarregar os pedidos após a exclusão.
+      } else {
+        Alert.alert("Erro ao deletar pedido");
+      }
+    } catch (error) {
+      Alert.alert("Erro ao deletar pedido", error.toString());
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      loadOrders();
+    }, [])
+  );
+
+
+  async function checkGetOrder(){
+      const orderFromGet = await getOrders();
+      console.log("[checkGetOrder] orderFromGet ", orderFromGet);
+    }
+
+
+  function addExampleItemToCart() {
+    const exampleItem = {
+      id: 9999999991, 
+      // name: "Exemplo de Pizza",
+      description: "Esta é uma pizza de exemplo.",
+      quantity: 1, // Quantidade de exemplo
+    };
+  
+    console.log("[addExampleItemToCart] exampleItem ", exampleItem);
+    // Adicione o item de exemplo ao carrinho
+    setCart([...cart, exampleItem]);
+  }
+  
+
+
+  const [cart, setCart] = useState([]); 
+  
+  async function createTestOrder() {
+    try {
+        const orderDate = new Date().toISOString();
+
+        addExampleItemToCart();
+        console.log("cart", cart);
+
+        // Use o método existente para criar o pedido
+        const success = await saveOrder(orderDate, cart);
+
+        if (success) {
+            Alert.alert("Pedido de teste criado com sucesso!");
+
+            // Verifique o último pedido inserido
+            const lastOrder = await getOrders();
+            if (lastOrder.length > 0) {
+                const orderId = lastOrder[lastOrder.length - 1].orderId;
+                const orderDetails = await getOrderById(orderId);
+                console.log("[createTestOrder] Last order details: ", orderDetails);
+            }
+
+            loadOrders(); // Recarregar a lista de pedidos após a criação
+        } else {
+            Alert.alert("Erro ao criar pedido de teste");
+        }
+    } catch (error) {
+        Alert.alert("Erro ao criar pedido de teste", error.toString());
+    }
+}
+
+  
+
+
+
+
+
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>ListSells!</Text>
-    </View>
+
+      <Button title="Criar Pedido de Teste" onPress={createTestOrder} />
+      <Button title="Deletar Pedidos" onPress={deleteAllOrders} />
+      <Button title="GET CHECK ORDERS" onPress={checkGetOrder} />
+      
+      {/* Listando os IDs dos pedidos */}
+      {orders.length > 0 ? (
+          <FlatList 
+              data={orders}
+              keyExtractor={(item, index) => item.orderId.toString()}
+              renderItem={({ item }) => (
+                  <View style={{ marginBottom: 10 }}>
+                      <Text>ID do Pedido: {item.orderId}</Text>
+                  </View>
+              )}
+          />
+      ) : (
+          <Text>Nenhum pedido para listar.</Text>
+      )}
+  </View>
+
   );
 }
--- screens/SellPizza/SellPizza.js
+++ screens/SellPizza/SellPizza.js
@@ -1,10 +1,111 @@
-import { Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button, FlatList, TouchableOpacity, Alert } from 'react-native';
 import styles from './styles';
+import { getAllPizzas, saveOrder, createOrdersTable, createOrderItemsTable } from '../../services/dbservices';
+import { useFocusEffect } from '@react-navigation/native';
+import { useCallback } from 'react'; // Adicione esta importação no topo
+
 
 export default function SellPizza() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>SellPizza!</Text>
-    </View>
-  );
+    const [pizzas, setPizzas] = useState([]);
+    const [cart, setCart] = useState([]);
+
+    useFocusEffect(
+      useCallback(() => {
+          loadPizzas();
+      }, [cart])
+    );
+
+  async function finalizeOrder() {
+    const orderDate = new Date().toISOString();
+    try {
+        await saveOrder(orderDate, cart);
+        Alert.alert('Pedido salvo com sucesso!');
+        setCart([]); // clear the cart after saving
+    } catch (error) {
+        Alert.alert('Erro ao salvar o pedido.', error.toString());
+    }
+  }
+  
+  
+
+  async function loadPizzas() {
+    try {
+        const pizzasList = await getAllPizzas();
+        setPizzas(pizzasList);
+
+        // Mova a validação do carrinho para cá
+        const validCartItems = cart.filter(cartItem => {
+            return pizzasList.some(pizza => pizza.id === cartItem.id);
+        });
+
+        if (validCartItems.length !== cart.length) {
+            Alert.alert("Atenção", "Alguns itens no seu carrinho foram removidos do banco de dados e foram excluídos do carrinho.");
+            setCart(validCartItems);
+        }
+    } catch (e) {
+        Alert.alert(e.toString());
+    }
+  }
+
+
+    function addToCart(pizzaToAdd) {
+        const found = cart.find(pizza => pizza.id === pizzaToAdd.id);
+        if (found) {
+            setCart(cart.map(pizza => 
+                pizza.id === pizzaToAdd.id 
+                ? { ...pizza, quantity: pizza.quantity + 1 } 
+                : pizza
+            ));
+        } else {
+            setCart([...cart, { ...pizzaToAdd, quantity: 1 }]);
+        }
+    }
+
+    function removeFromCart(pizzaId) {
+        const found = cart.find(pizza => pizza.id === pizzaId);
+        if (found && found.quantity > 1) {
+            setCart(cart.map(pizza => 
+                pizza.id === pizzaId 
+                ? { ...pizza, quantity: pizza.quantity - 1 } 
+                : pizza
+            ));
+        } else {
+            setCart(cart.filter(pizza => pizza.id !== pizzaId));
+        }
+    }
+
+    useEffect(() => {
+        loadPizzas();
+    }, []);
+
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Venda de Pizzas!</Text>
+
+            <FlatList 
+                data={pizzas}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={({ item }) => (
+                    <View style={{ marginBottom: 10 }}>
+                        <Text>{item.name}</Text>
+                        <Text>{item.description}</Text>
+                        <Button title="Adicionar ao Carrinho" onPress={() => addToCart(item)} />
+                    </View>
+                )}
+            />
+
+            <View style={{ marginTop: 20 }}>
+                <Text>Carrinho:</Text>
+                {cart.map((pizza, index) => (
+                    <View key={index} style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+                        <Text>{pizza.name} (x{pizza.quantity})</Text>
+                        <Button title="Remover" onPress={() => removeFromCart(pizza.id)} />
+                    </View>
+                ))}
+            </View>
+
+            <Button title="Finalizar Pedido" onPress={finalizeOrder} />
+        </View>
+    );
 }
