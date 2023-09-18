20d6b045e6d3c118b35d64c1cf8d4d8928385962
MarcioRK <marcio_rios_kunimura@hotmail.com>
2023-09-18 01:42:45 -0300

    edit button

--- screens/DisplayPizza/DisplayPizza.js
+++ screens/DisplayPizza/DisplayPizza.js
@@ -1,10 +1,20 @@
 import { Text, View } from 'react-native';
 import styles from './styles';
+// Importe o hook `useRoute` no início do arquivo
+import { useRoute } from '@react-navigation/native';
 
-export default function HomeScreen() {
+// ...
+
+export default function DisplayPizza() {
+  // Use o hook useRoute para obter os parâmetros da rota
+  const route = useRoute();
+  const { pizza } = route.params;
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>DisplayPizza!</Text>
+      <Text>{pizza.name}</Text>
+      <Text>{pizza.description}</Text>
+      {/* Adicione quaisquer outros detalhes que deseja exibir */}
     </View>
   );
 }
